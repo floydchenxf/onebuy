c21452d46655cb4c56ee0af7228e44e53e416859
陈晓峰 <chenxiaofeng@chenxiaengdeMBP.lan>
2016-08-14 00:10:07 +0800

    支持数据

--- app/src/main/java/com/floyd/onebuy/ui/activity/InviteFriendActivity.java
+++ app/src/main/java/com/floyd/onebuy/ui/activity/InviteFriendActivity.java
@@ -1,7 +1,6 @@
 package com.floyd.onebuy.ui.activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
