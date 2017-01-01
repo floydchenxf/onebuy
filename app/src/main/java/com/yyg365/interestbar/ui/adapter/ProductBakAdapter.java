2edd44f86c4b1c9ab82db84c0ad60c9a4ba87b36
zjutcxf128 <zjutcxf128@gmail.com>
2017-01-01 14:56:15 +0800

    更新代码

--- app/src/main/java/com/yyg365/interestbar/ui/adapter/ProductBakAdapter.java
+++ app/src/main/java/com/yyg365/interestbar/ui/adapter/ProductBakAdapter.java
@@ -21,7 +21,7 @@ import java.util.List;
 /**
  * Created by floyd on 16-5-2.
  */
-public class ProductAdapter extends BaseAdapter {
+public class ProductBakAdapter extends BaseAdapter {
 
     private Context mContext;
 
@@ -29,7 +29,7 @@ public class ProductAdapter extends BaseAdapter {
 
     private ImageLoader imageLoader;
 
-    public ProductAdapter(Context context, List<WinningInfo> args) {
+    public ProductBakAdapter(Context context, List<WinningInfo> args) {
         this.imageLoader = ImageLoaderFactory.createImageLoader();
         mList.addAll(args);
         mContext = context;
@@ -71,21 +71,26 @@ public class ProductAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         final ViewHolder holder;
         if (convertView == null) {
-            convertView = View.inflate(mContext, R.layout.product_item, null);
+            convertView = View.inflate(mContext, R.layout.product_bak_item, null);
             holder = new ViewHolder();
             holder.productImage1 = (NetworkImageView) convertView.findViewById(R.id.productImage1);
             holder.productImage2 = (NetworkImageView) convertView.findViewById(R.id.productImage2);
+            holder.productImage3 = (NetworkImageView) convertView.findViewById(R.id.productImage3);
 
             holder.layout1 = convertView.findViewById(R.id.productLayout1);
             holder.layout2 = convertView.findViewById(R.id.productLayout2);
+            holder.layout3 = convertView.findViewById(R.id.productLayout3);
 
             holder.productNameView1 = (TextView) convertView.findViewById(R.id.productNameView1);
             holder.productNameView2 = (TextView) convertView.findViewById(R.id.productNameView2);
+            holder.productNameView3 = (TextView) convertView.findViewById(R.id.productNameView3);
 
             holder.line1 = convertView.findViewById(R.id.line1);
+            holder.line2 = convertView.findViewById(R.id.line2);
 
             holder.typeIconView1 = (ImageView) convertView.findViewById(R.id.type_icon_view1);
             holder.typeIconView2 = (ImageView) convertView.findViewById(R.id.type_icon_view2);
+            holder.typeIconView3 = (ImageView) convertView.findViewById(R.id.type_icon_view3);
 
             convertView.setTag(holder);
         } else {
@@ -94,9 +99,10 @@ public class ProductAdapter extends BaseAdapter {
 
         holder.productImage1.setDefaultImageResId(R.drawable.tupian);
         holder.productImage2.setDefaultImageResId(R.drawable.tupian);
+        holder.productImage3.setDefaultImageResId(R.drawable.tupian);
 
-        int start = position * 2;
-        int end = position * 2 + 1;
+        int start = position * 3;
+        int end = position * 3 + 2;
 
 
         if (mList.size() - 1 >= end) {
@@ -137,6 +143,7 @@ public class ProductAdapter extends BaseAdapter {
                     mContext.startActivity(it);
                 }
             });
+            holder.line2.setVisibility(View.VISIBLE);
 
             holder.typeIconView2.setVisibility(View.GONE);
             if (info2.productType == 1) {
@@ -147,6 +154,28 @@ public class ProductAdapter extends BaseAdapter {
                 holder.typeIconView2.setImageResource(R.drawable.hun_icon);
             }
 
+            final WinningInfo info3 = mList.get(start + 2);
+            holder.productImage3.setImageUrl(info3.productUrl, imageLoader);
+            holder.productNameView3.setText(info3.title);
+            holder.layout3.setVisibility(View.VISIBLE);
+            holder.layout3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent it = new Intent(mContext, WinningDetailActivity.class);
+                    it.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    it.putExtra(WinningDetailActivity.PRODUCT_ID, info3.productId);
+                    mContext.startActivity(it);
+                }
+            });
+
+            holder.typeIconView3.setVisibility(View.GONE);
+            if (info3.productType == 1) {
+                holder.typeIconView3.setVisibility(View.VISIBLE);
+                holder.typeIconView3.setImageResource(R.drawable.ten_icon);
+            } else if (info3.productType == 2) {
+                holder.typeIconView3.setVisibility(View.VISIBLE);
+                holder.typeIconView3.setImageResource(R.drawable.hun_icon);
+            }
         } else {
             final WinningInfo info1 = mList.get(start);
             holder.productImage1.setImageUrl(info1.getProductUrl(), imageLoader);
@@ -173,9 +202,43 @@ public class ProductAdapter extends BaseAdapter {
                 holder.typeIconView1.setImageResource(R.drawable.hun_icon);
             }
 
-            holder.layout2.setVisibility(View.INVISIBLE);
-            holder.layout2.setOnClickListener(null);
-            holder.typeIconView2.setVisibility(View.GONE);
+            if (mList.size() - 1 < start + 1) {
+                holder.layout2.setVisibility(View.INVISIBLE);
+                holder.layout3.setVisibility(View.INVISIBLE);
+                holder.line2.setVisibility(View.GONE);
+                holder.layout2.setOnClickListener(null);
+                holder.layout3.setOnClickListener(null);
+                holder.typeIconView2.setVisibility(View.GONE);
+                holder.typeIconView3.setVisibility(View.GONE);
+            } else {
+                final WinningInfo info2 = mList.get(start + 1);
+                holder.productImage2.setImageUrl(info2.productUrl, imageLoader);
+                holder.productNameView2.setText(info2.title);
+                holder.layout2.setVisibility(View.VISIBLE);
+                holder.layout2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent it = new Intent(mContext, WinningDetailActivity.class);
+                        it.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        it.putExtra(WinningDetailActivity.PRODUCT_ID, info2.productId);
+                        mContext.startActivity(it);
+                    }
+                });
+                holder.line2.setVisibility(View.VISIBLE);
+
+                holder.typeIconView2.setVisibility(View.GONE);
+                if (info2.productType == 1) {
+                    holder.typeIconView2.setVisibility(View.VISIBLE);
+                    holder.typeIconView2.setImageResource(R.drawable.ten_icon);
+                } else if (info2.productType == 2) {
+                    holder.typeIconView2.setVisibility(View.VISIBLE);
+                    holder.typeIconView2.setImageResource(R.drawable.hun_icon);
+                }
+
+                holder.layout3.setVisibility(View.INVISIBLE);
+                holder.layout3.setOnClickListener(null);
+                holder.typeIconView3.setVisibility(View.GONE);
+            }
         }
 
         return convertView;
@@ -184,17 +247,22 @@ public class ProductAdapter extends BaseAdapter {
     public static class ViewHolder {
         private NetworkImageView productImage1;
         private NetworkImageView productImage2;
+        private NetworkImageView productImage3;
 
         private TextView productNameView1;
         private TextView productNameView2;
+        private TextView productNameView3;
 
         private View layout1;
         private View layout2;
+        private View layout3;
 
         private View line1;
+        private View line2;
 
         private ImageView typeIconView1;
         private ImageView typeIconView2;
+        private ImageView typeIconView3;
 
 
     }
