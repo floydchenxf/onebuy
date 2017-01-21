e298500abf2046012309a945b555f81f32b12f44
zjutcxf128 <zjutcxf128@gmail.com>
2017-01-21 16:25:10 +0800

    加固指令

--- app/apksigner.sh
+++ app/apksigner.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ## 输入加密apk目录
-jarsigner -verbose -keystore onebuy.keystore -signedjar onebuy.apk $1 diamond
+jarsigner -verbose -keystore ./interestbar.keystore -signedjar ./onebuy.apk $1 interestbar.keystore
