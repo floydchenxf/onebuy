24c386ccafc53ff451d254936b0df98875ac266b
zjutcxf128 <zjutcxf128@gmail.com>
2017-01-25 15:22:09 +0800

    解决首页广告的切换

--- app/src/main/java/com/yyg365/interestbar/ui/MainActivity.java
+++ app/src/main/java/com/yyg365/interestbar/ui/MainActivity.java
@@ -77,6 +77,8 @@ public class MainActivity extends FragmentActivity implements BackHandledInterfa
     private List<Fragment> fragments = new ArrayList<Fragment>();
     private FragmentTabAdapter tabAdapter;
 
+    private IndexFragment indexFragment;
+
     private long exitTime = 0;
 
     private TextView redRotNumView;
@@ -111,9 +113,9 @@ public class MainActivity extends FragmentActivity implements BackHandledInterfa
         }
 
         EventBus.getDefault().register(this);
-        fragments.add(new IndexFragment());
+        indexFragment = new IndexFragment();
+        fragments.add(indexFragment);
         fragments.add(new AllProductFragemnt());
-//        fragments.add(new NewOwnerFragment());
         fragments.add(new PawnFragment());
         fragments.add(new BuyCarFragment());
         fragments.add(new MyFragment());
@@ -129,6 +131,11 @@ public class MainActivity extends FragmentActivity implements BackHandledInterfa
         tabAdapter.setOnRgsExtraCheckedChangedListener(new FragmentTabAdapter.OnRgsExtraCheckedChangedListener() {
             @Override
             public void OnRgsExtraCheckedChanged(RadioGroup radioGroup, int checkedId, int index) {
+                if (index == 0) {
+                    indexFragment.startIndexView();
+                } else {
+                    indexFragment.stopIndexView();
+                }
             }
         });
 
